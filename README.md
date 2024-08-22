b8e9260f7ba3dfb4c8bc0b09da6dd8e4bbadb434
Roma K. <804157+romakuchukov@users.noreply.github.com>
2024-08-22 02:47:13 -0400

    git init

---
+++ README.md
@@ -0,0 +1,10 @@
+run:
+
+```
+1.  npm i
+1.5 npx playwright install (might need to run this)
+2.  npx tsc (compile)
+3.  node dist/index.js (execute)
+```
+
+You can run last 2 steps as one `npx tsc && node dist/index.js`
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import playwright from "playwright";
+
+const TIMEOUT = 200;
+
+(async () => {
+  // store text in a variable
+  let elementText = "";
+  // since i don't know which browser will be available, so let's loop through some of them, though it's not necessary
+  // it needs only one browser to get the text from the page ["chromium", "firefox", "webkit"] -> ["chromium"] or the loop can be refactored away
+  for (const browserType of ["chromium", "firefox", "webkit"]) {
+    const browser = await playwright[browserType].launch({ headless: true });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto(
+      "https://odcom-69a2f5f05adbd3cd6ee6b80e9fbf1a2f.read.overdrive.com/"
+    );
+
+    elementText = await page.$eval(
+      ".title-page__description p",
+      (el: HTMLParagraphElement) => el.textContent
+    );
+    // increase the timeout if there is an issue with server blocking you
+    await page.waitForTimeout(TIMEOUT);
+    await browser.close();
+  }
+  // write data to a file
+  fs.writeFile("data.txt", elementText, (err) => {
+    if (err) return console.log(err);
+    console.log("The file was saved!");
+  });
+})();
