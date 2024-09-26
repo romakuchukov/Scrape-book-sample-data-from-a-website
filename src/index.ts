7b8d1856dca40da6c162a9abb219927c9786b7f3
Roma K. <804157+romakuchukov@users.noreply.github.com>
2024-09-26 17:11:43 -0400

    full html + top level await + cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,26 @@
 import fs from "fs";
-import { chromium, Locator, Page } from "playwright";
+import { chromium } from "playwright";
 
-const TIMEOUT = 200;
-const FILE = "data.json";
-const TITLE_SELECTOR = "div.title-page__info";
-const BODY_SELECTOR = "div.title-page__description p";
-const URL =
-  "https://odcom-69a2f5f05adbd3cd6ee6b80e9fbf1a2f.read.overdrive.com/";
+const url = new URL(
+  "https://odcom-69a2f5f05adbd3cd6ee6b80e9fbf1a2f.read.overdrive.com"
+);
+const FILE = `${url.hostname}.html`;
 
-// helper function to get text from the page
-async function getText(
-  page: Page,
-  selector: string,
-  options: { has: Locator }
-) {
-  return await page
-    .locator(selector, options)
-    .evaluate((elements) =>
-      [...elements.childNodes]
-        .filter((element) => element.nodeType === Node.TEXT_NODE)
-        .map((element) => element.textContent)
-    );
-}
+console.log("Starting the scraping process...");
 
-(async () => {
-  console.log("Starting the scraping process...");
+// launch browser and create a new page
+const browser = await chromium.launch({ headless: true });
+const context = await browser.newContext();
+const page = await context.newPage();
+// fetch the page
+await page.goto(url.origin);
 
-  const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.goto(URL);
+// get html content
+const html = await page.content();
 
-  const titleData = await page
-    .locator(TITLE_SELECTOR)
-    .allInnerTexts()
-    .then((text) => text.join("\n").split("\n"));
-
-  const metaData = await page.locator(BODY_SELECTOR).allTextContents();
-  metaData.shift();
-
-  // storing data in an object
-  const text = {
-    meta: [titleData.shift(), ...metaData],
-    text: [
-      ...(await getText(page, `${BODY_SELECTOR} b`, {
-        has: page.locator("i"),
-      })),
-      ...(await getText(page, BODY_SELECTOR, { has: page.locator("b") })),
-    ],
-  };
-
-  // increase the timeout if there is an issue with server blocking you
-  await page.waitForTimeout(TIMEOUT);
-  await browser.close();
-  // write data to a file
-  fs.writeFile(FILE, JSON.stringify(text), (err) => {
-    if (err) return console.log(err);
-    console.log("The file was saved!");
-  });
-})();
+await browser.close();
+// write data to a file
+fs.writeFile(FILE, html, (err) => {
+  if (err) return console.log(err);
+  console.log("The file was saved!");
+});
