20c5aa2bc4c4f3c204e9da491660b2129d120fc1
Roma K. <804157+romakuchukov@users.noreply.github.com>
2024-10-02 01:10:47 -0400

    comments

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,38 @@
 import fs from "fs";
 import { chromium } from "playwright";
 
+// if some content is missing, increase the TIMEOUT
+const TIMEOUT = 500;
 const url = new URL(
   "https://odcom-69a2f5f05adbd3cd6ee6b80e9fbf1a2f.read.overdrive.com/"
 );
 const FILE = `${url.hostname}.json`;
 
 console.log("Starting the scraping process...");
-
+// launch the browser
 const browser = await chromium.launch();
 const context = await browser.newContext();
 const page = await context.newPage();
-
+// go to the main page
 await page.goto(url.origin);
-
+// grab the redirect url
 const redirecturl = await page
   .locator("#sample-holder")
   .getAttribute("data-redirecturl");
 
+// redirect to json
 let response = await page.goto(redirecturl);
 const { src } = await response.json();
-
+// go to the sample book
 await page.goto(src);
 
+// wait for the inline frame to finish loading so we can quickly go through the content
+// also helps to avoid error handling
+const iframe = page.locator("#LENS_CMPT_0 iframe");
+await iframe.waitFor({ state: "visible" });
+
+// get the content
 const content = await page
-  // wait for selector
   .locator(".chapter-bar-next-button", { has: page.locator("span") })
   .evaluate((element: HTMLElement) => {
     return new Promise((resolve) => {
@@ -44,11 +52,13 @@ const content = await page
           resolve(data);
         }
         counter++;
-      }, 1000);
+      }, TIMEOUT);
     });
   });
 
 console.log("Writing to file then exiting...");
-fs.writeFileSync(FILE, JSON.stringify(content));
-
-await browser.close();
+// write the content to a file
+fs.writeFile(FILE, JSON.stringify(content), async (err) => {
+  await browser.close();
+  if (err) return console.error(err);
+});
